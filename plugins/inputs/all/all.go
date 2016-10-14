59d69b4cde9abdeb4052681bb40c1575561e92ea
Andrew Reslan <andrew@couchbase.com>
2016-10-14 14:56:05 +0100

    Removed gateload input plugin, gateload now exposes stats via statsd client

--- plugins/inputs/all/all.go
+++ plugins/inputs/all/all.go
@@ -20,7 +20,6 @@ import (
 	_ "github.com/influxdata/telegraf/plugins/inputs/elasticsearch"
 	_ "github.com/influxdata/telegraf/plugins/inputs/exec"
 	_ "github.com/influxdata/telegraf/plugins/inputs/filestat"
-	_ "github.com/influxdata/telegraf/plugins/inputs/gateload"
 	_ "github.com/influxdata/telegraf/plugins/inputs/graylog"
 	_ "github.com/influxdata/telegraf/plugins/inputs/haproxy"
 	_ "github.com/influxdata/telegraf/plugins/inputs/hddtemp"
